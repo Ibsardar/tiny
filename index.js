37691a0a44c793bb0b7288a6be6fc42db6a7a385
Ibrahim Sardar <isardar@funaiservice.com>
2020-09-13 18:33:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+console.log('yay npm pkg "tiny" is working!');
+
 module.exports = function tiny(string) {
     // A tiny function that removes all spaces from a string.
     if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
