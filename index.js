15acfaff47ed845de0d44b3887b85eab4c030700
Ibrahim Sardar <isardar@funaiservice.com>
2020-09-13 09:33:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const tiny = function tiny(string) {
-  // A tiny function that removes all spaces from a string.
-  if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
-  return string.replace(/\s/g, "");
-};
-
-module.exports = {
-  tiny
+module.exports = function tiny(string) {
+    // A tiny function that removes all spaces from a string.
+    if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+    return string.replace(/\s/g, "");
 };
